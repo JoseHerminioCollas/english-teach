c5391f6ca213f8ce6140de8b420818a4f3ea5a6c
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2026-08-28 15:09:36 -0500

    update config to build in docs folder

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  distDir: "docs",
   output: "export",
   images: { unoptimized: true }, // disables server image optimization
   trailingSlash: true,
