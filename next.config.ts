7c533b482f8f6538f2369782c7ced7998da42b20
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2026-08-28 08:46:34 -0500

    update config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/english-teach", // must match your repo name
   images: { unoptimized: true }, // disables server image optimization
 };
 
